1358a6e7a789b586f891dd9e45d4fd8376dbf9a6
shmeka <shmekah@gmail.com>
2022-05-05 11:56:12 -0400

    password gen & email val update

--- project-nodejs/app.js
+++ project-nodejs/app.js
@@ -1,2 +1,16 @@
-console.log("Hello World today again \n")
+//console.log("Hello World today again \n")
+
+// ask user if they want to generate a password or validate an email format
+
+
+var generator = require('generate-password');
+
+var password = generator.generate({
+    length:10,
+    numbers: true
+})
+
+console.log(password)
+
+
 
---
+++ project-nodejs/emailVal.js
@@ -0,0 +1,10 @@
+function generatePassword() {
+
+    
+    var generator = require('generate-password');
+
+    var password = generator.generate({
+        length:10,
+        numbers: true
+})
+}
